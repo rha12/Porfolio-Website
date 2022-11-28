d53e99d12c5f39966a1e88aa1e1875db49da2641
Raja Haider Ali <rajahaiderali8@gmail.com>
2022-11-28 22:06:25 +0500

    hamburger menu added

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+const hamburgerContainer= document.querySelector('#header__main-nav');
+const hamburger= document.querySelector('.header__main-nav--hamburger');
+const links= document.querySelectorAll('.header__main-nav--links li');
+
+hamburgerContainer.addEventListener('click', () => {
+    hamburgerContainer.classList.toggle('clicked');
+    links.forEach((link)=>{
+        link.classList.toggle('fade')
+    })
+});
+
+
